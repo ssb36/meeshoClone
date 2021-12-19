ea497d66c8f0d813cade89c5d1f7ecb43b95046f
Himanshu0316 <hsupadhyay316@gmail.com>
2021-12-19 09:20:45 +0530

    becomesplier.js

---
+++ becomesplier.js
@@ -0,0 +1,7 @@
+document.querySelector("#btn1").addEventListener("click" , function(){
+    window.location.href = "signin.html";
+    
+});
+document.querySelector("#btn2").addEventListener("click" , function(){
+    window.location.href = "login.html";
+});
